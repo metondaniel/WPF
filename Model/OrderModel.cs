d058d2324846694d0d10f66e065d8302a4b89d32
metondaniel <meton.daniel@gmail.com>
2020-03-31 14:43:38 -0300

    Add files via upload

---
+++ Model/OrderModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp.Model
+{
+    // model of the Order. 
+    // not inherited from BaseModel because no orders are placed in this app
+
+    public class OrderModel
+    {
+        public int OrderId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public DateTime RequiredDate { get; set; }
+        public float Freight { get; set; }
+
+        public MemberModel Member { get; set; }
+        public ObservableCollection<OrderDetailModel> OrderDetails { get; set; }
+    }
+}
---
+++ Model/Provider/IProvider.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp.Model.Provider
+{
+    public interface IProvider
+    {
+        bool Login(string userName, string password);
+        void Logout();
+
+        ObservableCollection<MemberModel> GetMembers(string sortExpression);
+        MemberModel GetMember(int memberId);
+
+        void AddMember(MemberModel member);
+        void UpdateMember(MemberModel member);
+        void DeleteMember(int memberId);
+
+        ObservableCollection<OrderModel> GetOrders(int memberId);
+    }
+}
